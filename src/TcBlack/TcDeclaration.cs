b4ab8122c380daa263e2d0f5f9752c40f6e46d0d
Roald Ruiter <roaldruiter@gmail.com>
2020-09-14 11:06:42 +0200

    Added extension project which can format structured text.

---
+++ src/TcBlack/TcDeclaration.cs
@@ -0,0 +1,58 @@
+﻿namespace TcBlack
+{
+    public struct TcDeclaration
+    {
+        public TcDeclaration(
+            string name,
+            string allocation,
+            string dataType,
+            string initialization,
+            string comment
+        )
+        {
+            Name = name;
+            Allocation = allocation;
+            DataType = dataType;
+            Initialization = initialization;
+            Comment = comment;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            var other = (TcDeclaration)obj;
+            return 
+                Name == other.Name 
+                && Allocation == other.Allocation 
+                && DataType == other.DataType
+                && Initialization == other.Initialization 
+                && Comment == other.Comment
+            ;
+        }
+
+        public static bool operator==(TcDeclaration obj1, TcDeclaration obj2)
+        {
+            if (obj1 == null)
+            {
+                return obj2 == null;
+            }
+
+            return obj1.Equals(obj2);
+        }
+
+        public static bool operator !=(TcDeclaration obj1, TcDeclaration obj2)
+        {
+            return !(obj1 == obj2);
+        }
+
+        public string Name { get; }
+        public string Allocation { get; }
+        public string DataType { get; }
+        public string Initialization { get; }
+        public string Comment { get; }
+    }
+}
