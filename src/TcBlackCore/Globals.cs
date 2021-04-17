2416a60a61fbdb184c9ad9d5cda41c2f0c55d584
Roald Ruiter <roaldruiter@gmail.com>
2021-04-17 08:32:02 +0200

    Supressing CA2211 Non-constant fields should not be visible for globals.

--- src/TcBlackCore/Globals.cs
+++ src/TcBlackCore/Globals.cs
@@ -2,7 +2,17 @@
 {
     public static class Globals
     {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage(
+            "Microsoft.Usage",
+            "CA2211: Non-constant fields should not be visible.",
+            Justification = "Don't know better alternative."
+        )]
         public static string lineEnding = "";
+        [System.Diagnostics.CodeAnalysis.SuppressMessage(
+            "Microsoft.Usage",
+            "CA2211: Non-constant fields should not be visible.",
+            Justification = "Don't know better alternative."
+        )]
         public static string indentation = "";
     }
 }
